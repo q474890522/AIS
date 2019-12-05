d2ee1bc2c9672f846705b31713fd5c742781ff8d
Sixus <782516426@qq.com>
2019-12-05 17:18:29 +0800

    重新用了iOCR 合并代码后getAuth(String ak, String sk)函数内的try错误，连接错误connection.connetct();

--- app/src/main/java/com/example/ais/MainActivity.java
+++ app/src/main/java/com/example/ais/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -25,6 +27,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -35,10 +38,24 @@ import android.widget.Toast;
 
 import com.example.ais.R;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import com.example.ais.BaiduiOCR;
+import com.example.ais.Base64Util;
+
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity implements MainContract.View{
@@ -50,8 +67,6 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
     private Button button;
     File mTmpFile;
     Uri imageUri;
-    BottomNavigationView bottomNavigationView;
-    private MainPresenter mPresenter;
     private LinearLayout save;
     private LinearLayout takepic;
     private LinearLayout commit;
@@ -72,15 +87,27 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
         imageView = findViewById(R.id.imageView);
         textView = findViewById(R.id.textView);
         button = findViewById(R.id.TakePhoto);
-        mPresenter = new MainPresenter(this);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //takePhoto();
                 Resources r = mContext.getResources();
                 Bitmap bmp = BitmapFactory.decodeResource(r, R.drawable.test);
-                mPresenter.getRecognitionResultByImage(bmp);
-                imageView.setImageBitmap(bmp);
+                Log.d("bmp1", bmp.toString());
+                Log.d("bmp2", getBitmapByte(bmp).toString());
+                //textView.setText(getBitmapByte(bmp).toString());
+                textView.setText(getResult(getBitmapByte(bmp)));
+                //getResult(getBitmapByte(bmp));
+                //String rawPath = "android.resource://" + getPackageName() + "/" + R.raw.test;
+                //Uri uri=Uri.parse(rawPath);
+                //imageView.setImageURI(uri);
+                //String path =  uritopath.getFilePathByUri(getApplicationContext(),uri);
+                //textView.setText(uri.toString() + "1" + uritopath.getFilePathByUri(getApplicationContext(),uri));
+                //String path = "file:///android_asset/test.jpg";
+                //Bitmap bmp=BitmapFactory.decodeFile(path);
+                //BaiduiOCR.getResult(path);
+                //mPresenter.getRecognitionResultByImage(bmp);
+                //imageView.setImageBitmap(bmp);
             }
         });
         save = (LinearLayout)findViewById(R.id.save);
@@ -136,15 +163,12 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
         }
 
         Intent intent = new Intent();
-        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-        String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/img";
-        if (new File(path).exists()) {
-            try {
-                new File(path).createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/img/";
+        File file = new File(path);
+        if (!file.exists()) {
+                new File(path).mkdirs();
         }
+        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
         String filename = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         mTmpFile = new File(path, filename + ".jpg");
         mTmpFile.getParentFile().mkdirs();
@@ -179,9 +203,118 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAMERA_REQUEST_CODE) {
             Bitmap photo = BitmapFactory.decodeFile(mTmpFile.getAbsolutePath());
-            mPresenter.getRecognitionResultByImage(photo);
+            //mPresenter.getRecognitionResultByImage(photo);
             imageView.setImageBitmap(photo);
         }
     }
+
+
+    public byte[] getBitmapByte(Bitmap bitmap){   //将bitmap转化为byte[]类型也就是转化为二进制
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+        return out.toByteArray();
+    }
+
+    public String getResult(byte[] imgData) {
+        /**
+         * ��Ҫ��ʾ���������蹤����
+         * FileUtil,Base64Util,HttpUtil,GsonUtils���
+         * https://ai.baidu.com/file/658A35ABAB2D404FBF903F64D47C1F72
+         * https://ai.baidu.com/file/C8D81F3301E24D2892968F09AE1AD6E2
+         * https://ai.baidu.com/file/544D677F5D4E4F17B4122FBD60DB82B3
+         * https://ai.baidu.com/file/470B3ACCA3FE43788B5A963BF0B625F3
+         * ����
+         */
+
+        String recogniseUrl = "https://aip.baidubce.com/rest/2.0/solution/v1/iocr/recognise";
+        String result1 = null;
+
+        //String filePath = "D:\\Workspace\\Javawork\\baiduocr\\res\\test.jpg";
+        try {
+            //byte[] imgData = FileUtil.readFileByBytes(filePath);
+            String imgStr = Base64Util.encode(imgData);
+            // ����ģ�����
+            String recogniseParams = "templateSign=7d59619c3d1e98c44f61a98bd81994a7&image=" + URLEncoder.encode(imgStr, "UTF-8");
+            // �������������
+            //String classifierParams = "classifierId=your_classfier_id&image=" + URLEncoder.encode(imgStr, "UTF-8");
+            Log.d("bmp3",imgData.toString());
+
+            Log.d("bmp4",imgStr);
+
+            String accessToken = getAuth();
+            Log.d("bmp5", accessToken);
+
+
+
+            String result = HttpUtil.post(recogniseUrl, accessToken, recogniseParams);
+            result1 = result;
+            Log.d("bmp6", imgStr);
+
+            // String result = HttpUtil.post(recogniseUrl, accessToken, classifierParams);
+            //System.out.println(result);
+            //getJson.Json(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return result1;
+    }
+
+    public static String getAuth() {
+
+        String clientId = "G7AqxHlhGNrb3t8tZwzU1MVf";
+
+        String clientSecret = "pFuCgB4HscigzVBs7lFOGqhZFBTGB5xa";
+
+        return getAuth(clientId, clientSecret);
+    }
+
+
+    public static String getAuth(String ak, String sk) {
+
+        String authHost = "https://aip.baidubce.com/oauth/2.0/token?";
+        String getAccessTokenUrl = authHost
+
+                + "grant_type=client_credentials"
+
+                + "&client_id=" + ak
+
+                + "&client_secret=" + sk;
+        Log.d("bmp7",getAccessTokenUrl);
+        try {
+            URL realUrl = new URL(getAccessTokenUrl);
+
+            HttpURLConnection connection = (HttpURLConnection) realUrl.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+
+            Map<String, List<String>> map = connection.getHeaderFields();
+
+            for (String key : map.keySet()) {
+                System.err.println(key + "--->" + map.get(key));
+            }
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String result = "";
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+
+            System.err.println("result:" + result);
+            JSONObject jsonObject = new JSONObject(result);
+            String access_token = jsonObject.getString("access_token");
+
+            return access_token;
+        } catch (Exception e) {
+            //System.err.printf("get token fail!");
+            Log.d("bmp9","get token fail!");
+            e.printStackTrace(System.err);
+        }
+        return null;
+    }
+
+
 
 }
